fdd20626e5353f187eb513d6db42db1d905aac14
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-08-12 00:19:37 -0300

    Iniciado app de labs

--- susana/settings.py
+++ susana/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'login.apps.LoginConfig',
     'disciplinas.apps.DisciplinasConfig',
+    'labs.apps.LabsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
