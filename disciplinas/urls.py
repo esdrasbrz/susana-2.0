f5bc7c44902e08dfaa3d8d3ddc514c7e2173ab57
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-08-12 10:10:09 -0300

    Inserindo novas disciplinas

--- disciplinas/urls.py
+++ disciplinas/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'disciplinas'
 urlpatterns = [
     url(r'^$', views.disciplinas, name='disciplinas'),
-    url(r'^/novo/$', views.nova_disciplina, name='nova_disciplina'),
+    url(r'^novo/$', views.nova_disciplina, name='nova_disciplina'),
+    url(r'^novo/salvar/$', views.salvar_disciplina, name='salvar_disciplina'),
 ]
--- disciplinas/views.py
+++ disciplinas/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.db import transaction
+from django.contrib import messages
 from login.user_tests import *
 from .models import *
+from utils.arquivos import *
 
 """
 Lista todas as disciplinas para possíveis alterações
@@ -22,3 +25,25 @@ Cria uma nova disciplina para inserção
 def nova_disciplina(request):
     # renderiza o diálogo de alterar disciplina
     return render(request, 'disciplinas/alterarDisciplina.html', {'nova_disciplina': True})
+
+"""
+Salva a disciplina no BD e cria os arquivos
+"""
+@login_required(login_url='/login/')
+@user_passes_test(is_superuser, login_url='/login/')
+@transaction.atomic
+def salvar_disciplina(request):
+    # cria a disciplina com os dados do form
+    disciplina = Disciplinas(codigo=request.POST['codigo'],
+                             turma=request.POST['turma'],
+                             descricao=request.POST['descricao'])
+
+    # salva a disciplina no BD
+    disciplina.save()
+
+    # cria o diretório para a disciplina
+    cria_disciplina(str(disciplina))
+
+    # renderiza à listagem
+    messages.success(request, 'Disciplina salva com sucesso!')
+    return disciplinas(request)
