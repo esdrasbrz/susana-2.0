ae6424777e790a2555e079fb103b642fcf4f0307
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-08-12 09:29:28 -0300

    Função para excluir disciplina e lab

--- utils/arquivos.py
+++ utils/arquivos.py
@@ -4,12 +4,12 @@
     Obs.: Script funciona em python 2.7
 """
 import os
-import commands
+import subprocess
 import re
+from django.conf import settings
 
 # PATHs usados no SuSana
-SUSANA_FILES = "/opt/tomcat/webapps/susana-files/" # server
-#SUSANA_FILES = "/home/esdrasbrz/Projects/java/susana/susana-files/" # developer
+SUSANA_FILES = "%s/susana-files/" % settings.BASE_DIR
 
 # cria o diretorio de um novo usuario
 def new_user(session_id, disciplina, lab):
@@ -38,9 +38,9 @@ def compilar(fileName, disciplina, lab, session_id):
             if filtro.search(fonte) is not None:
                 return u"error: Você não tem permissão para executar um comando do sistema"
 
-    ret = commands.getoutput("gcc -std=c99 -pedantic -Wall -lm %s -o %s" %(fileName, path+session_id+".out"))
+    ret = subprocess.getoutput("gcc -std=c99 -pedantic -Wall -lm %s -o %s" %(fileName, path+session_id+".out"))
 
-    return unicode(ret, "utf-8")
+    return ret
 
 # Seta as permissoes para o executavel
 def set_permissao(fileName, disciplina, lab):
@@ -68,12 +68,12 @@ def testar(fileName, disciplina, lab, num):
         return "Segmentation fault!"
 
     # compara a saida
-    diff = commands.getoutput("diff %sarq%02d %sarq%02d.res" %(path_out + fileName, int(num), path_testes, int(num)))
+    diff = subprocess.getoutput("diff %sarq%02d %sarq%02d.res" %(path_out + fileName, int(num), path_testes, int(num)))
 
     # apagar o arquivo de saida
     os.system("cd %s && rm %sarq%02d" %(path_out, fileName, int(num)))
 
-    return unicode(diff, "utf-8")
+    return diff
 
 # Apaga o executavel e o codigo fonte dos arquivos
 def limpar(session_id, disciplina, lab):
@@ -97,7 +97,6 @@ def cria_lab(disciplina, lab, url_testes, qtd_testes):
 
     # cria os diretorios testes e out
     os.system("cd %s && mkdir %s" %(path_lab, "testes"))
-    os.system("cd %s && mkdir %s" %(path_lab, "out"))
 
     # define o path testes
     path_testes = path_lab + "testes/"
@@ -105,4 +104,13 @@ def cria_lab(disciplina, lab, url_testes, qtd_testes):
     # realiza o download dos arquivos de teste
     for i in range(1, qtd_testes+1):
         os.system("cd %s && curl -O -k %sarq%02d.in" %(path_testes, url_testes, i)) # entrada
-    	os.system("cd %s && curl -O -k %sarq%02d.res" %(path_testes, url_testes, i)) # saida
+        os.system("cd %s && curl -O -k %sarq%02d.res" %(path_testes, url_testes, i)) # saida
+
+# apaga os diretórios e arquivos do lab
+def apaga_lab(disciplina, lab):
+    # apaga os diretórios e subdiretórios
+    os.system("cd %s && rm -rf %s" % (SUSANA_FILES + disciplina, lab))
+
+# apaga a disciplina e todos os subdiretórios
+def apaga_disciplina(disciplina):
+    os.system('cd %s && rm -rf %s' % (SUSANA_FILES, disciplina))
