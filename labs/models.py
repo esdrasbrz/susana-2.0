7469f240279e825b09f15496ad7e649ae20cae6a
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-08-12 00:22:10 -0300

    Criado tabela de Labs

--- labs/models.py
+++ labs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from disciplinas.models import Disciplinas
 
-# Create your models here.
+
+"""
+Tabela de Labs
+"""
+class Labs(models.Model):
+    # relacionamento com disciplinas
+    disciplina = models.ForeignKey(Disciplinas, on_delete=models.CASCADE)
+
+    nome = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nome
