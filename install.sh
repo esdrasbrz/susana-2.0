28e5fd165866da695c36feaa13dff1ddecc6380c
Esdras Rodrigues do Carmo <esdrasbrz@gmail.com>
2016-11-24 18:08:37 -0200

    Adicionado sudo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-apt-get install curl
-apt-get install python3-pip
-pip3 install django
+sudo apt-get install curl
+sudo apt-get install python3-pip
+sudo pip3 install django
 
 python3 manage.py migrate
 python3 manage.py createsuperuser
